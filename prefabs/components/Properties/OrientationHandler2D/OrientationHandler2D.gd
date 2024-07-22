a2be203fabd4f953a69c3ecee65c5c367a14d678
Totsnuk <evanmaywald2@gmail.com>
2024-07-21 23:16:42 -0500

    bruh

--- prefabs/components/Properties/OrientationHandler2D/OrientationHandler2D.gd
+++ prefabs/components/Properties/OrientationHandler2D/OrientationHandler2D.gd
@@ -1,3 +1,14 @@
-extends Node2D
+extends Resource
+class_name OrientationHandler2D
 
-# 
+class ShapeData:
+	var collision_shape: Node2D
+	var right_position: Vector2
+	var left_position: Vector2
+
+@export var collision_shape_array: Array[OrientationResource]
+# Changes all collisionshape2d positions to custom based on input
+
+# have a storable x/y for right and left facing, for each collisionshape2d of an array
+
+
